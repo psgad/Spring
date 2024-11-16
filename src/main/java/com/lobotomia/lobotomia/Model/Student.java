b02d5ec17ffaefbdde730d8151c27307e1b4efd7
psga <v27022020@gmail.com>
2024-11-16 16:24:43 +0300

    prak3

--- src/main/java/com/lobotomia/lobotomia/Model/Student.java
+++ src/main/java/com/lobotomia/lobotomia/Model/Student.java
@@ -1,48 +1,85 @@
 package com.lobotomia.lobotomia.Model;
 
-public class Student {
-    int id;
-    String FIO;
-    String group;
-    int course;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Value;
 
-    public Student(int id, String FIO, String group, int course) {
+@Entity
+@Table(name="student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "Длина должна быть от 2 до 10 символов")
+    String FIO;
+    @NotEmpty
+    @Size(min = 2, max = 10, message = "Длина должна быть от 2 до 10 символов")
+    String student_group;
+    @Min(1)
+    @Max(4)
+    int course;
+    @Value("${some.value}")
+    boolean isDeleted = false;
+
+    public Student() {}
+
+    public Student(Long id, String FIO, String group, int course) {
         this.id = id;
         this.FIO = FIO;
-        this.group = group;
+        this.student_group = group;
         this.course = course;
     }
 
-    public int getId() {
+    public Student(String FIO, String group, int course) {
+        this.FIO = FIO;
+        this.student_group = group;
+        this.course = course;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getFIO() {
+    public @NotEmpty @Size(min = 2, max = 50, message = "Длина должна быть от 2 до 10 символов") String getFIO() {
         return FIO;
     }
 
-    public void setFIO(String FIO) {
+    public void setFIO(@NotEmpty @Size(min = 2, max = 50, message = "Длина должна быть от 2 до 10 символов") String FIO) {
         this.FIO = FIO;
     }
 
-    public String getGroup() {
-        return group;
+    public @NotEmpty @Size(min = 2, max = 10, message = "Длина должна быть от 2 до 10 символов") String getGroup() {
+        return student_group;
     }
 
-    public void setGroup(String group) {
-        this.group = group;
+    public void setGroup(@NotEmpty @Size(min = 2, max = 10, message = "Длина должна быть от 2 до 10 символов") String student_group) {
+        this.student_group = student_group;
     }
 
+    @Min(1)
+    @Max(4)
     public int getCourse() {
         return course;
     }
 
-    public void setCourse(int course) {
-        if(course < 1 || course > 4) throw new IllegalArgumentException("Значение курса вышло за границы диапазона [1-4]");
+    public void setCourse(@Min(1) @Max(4) int course) {
         this.course = course;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
     }
 }
